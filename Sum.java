0ef7456543875765c0440294c705a8373ca551cd
Asim Bhatta <asimbhatta15@gmail.com>
2023-03-05 12:13:07 +0545

    Sum with scanner class

--- Sum.java
+++ Sum.java
@@ -1,5 +1,15 @@
 //Write a Java program to print the sum of two numbers.
 package CoreJava;
+import java.util.*;
 
 public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is "+sum);
+    }
 }
