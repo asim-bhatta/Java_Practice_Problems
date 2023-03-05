0294cf4f4b7c8db0921703caf9c4535cba051532
Asim Bhatta <asimbhatta15@gmail.com>
2023-03-05 11:44:49 +0545

    Commited

---
+++ Hello.java
@@ -0,0 +1,13 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+//Expected Output :
+//Hello
+//Alexandra Abramov
+
+package CoreJava;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Asim");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,5 @@
+//Write a Java program to print the sum of two numbers.
+package CoreJava;
+
+public class Sum {
+}
